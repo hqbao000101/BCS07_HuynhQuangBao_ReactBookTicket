7ab389462e1cd306d46c7f6bdeed2cd3472e937f
hqbao000101 <hbao592000@gmail.com>
2023-06-23 18:23:34 +0700

    feat: define-components-and-layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
-import './App.css';
+import "./App.css";
+import Form from "./Components/Form";
+import SeatGrid from "./Components/SeatGrid";
 
 function App() {
   return (
     <>
-      <h1 className='text-center py-5 text-warning'>MOVIE SEAT SELECTION</h1>
+      <div className="container">
+        <h1 className="text-center py-5 text-warning ">MOVIE SEAT SELECTION</h1>
+        <div className="content text-center bg-white bg-opacity-10 p-5 w-50 mx-auto">
+          <Form />
+          <SeatGrid />
+          <h3 className="my-4 text-black bg-white rounded-3 d-inline-block fw-normal p-3 fs-5 py-2">Confirm Selection</h3>
+          <table className="table bg-white text-black w-75 mx-auto" border={3}>
+            <thead>
+              <tr>
+                <td>Name</td>
+                <td>Numer of Seats</td>
+                <td>Seats</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <p className="text-center mt-5">
+          &copy; 2018 Movie Seat Selection. All rights Reserved | Design by
+          W3layouts
+        </p>
+      </div>
     </>
   );
 }
---
+++ src/Components/SeatGrid.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class SeatGrid extends Component {
+  render() {
+    return (
+      <div>SeatGrid</div>
+    )
+  }
+}
